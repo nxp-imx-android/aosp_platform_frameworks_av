be906c1c2968a0b04882a488909ea34e061758b5
xiahong.bao <xiahong.bao@nxp.com>
2018-09-27 13:46:02 +0800

    MA-12751 RTP/UDP Streaming can't play
    For RTP/UDP Streaming, mCachedSource is null,
    need to check this value before use it.
    
    Change-Id: I16751c6a5c167dc1ad2d862d758c92d04d4ed9bb
    Signed-off-by: xiahong.bao <xiahong.bao@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/6104
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/12476

--- media/libmediaplayerservice/nuplayer/GenericSource.cpp
+++ media/libmediaplayerservice/nuplayer/GenericSource.cpp
@@ -517,7 +517,8 @@ void NuPlayer::GenericSource::finishPrepareAsync() {
     }
 
     if (mIsStreaming) {
-        mCachedSource->resumeFetchingIfNecessary();
+        if (mCachedSource != NULL)
+            mCachedSource->resumeFetchingIfNecessary();
         mPreparing = true;
         schedulePollBuffering();
     } else {
@@ -901,7 +902,8 @@ status_t NuPlayer::GenericSource::dequeueAccessUnit(
             }
             if (track->mPackets->getAvailableBufferCount(&finalResult) < 2
                 && !mSentPauseOnBuffering && !mPreparing) {
-                mCachedSource->resumeFetchingIfNecessary();
+                if (mCachedSource != NULL)
+                    mCachedSource->resumeFetchingIfNecessary();
                 sendCacheStats();
                 mSentPauseOnBuffering = true;
                 sp<AMessage> notify = dupNotify();
