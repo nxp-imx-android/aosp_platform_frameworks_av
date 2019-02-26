nxp-imx-android/aosp_platform_frameworks_av

commit b4bddc3038f6097d8a9f5f62628f45b52c71080d

MA-13701 audio: disable IAudioFlinger timecheck in LPA mode
Timecheck is a thread to check whether IAudioFlinger
or IAudioPolicy's binder call timeout, disable
it in LPA mode to fix below crash after system resume:
    F DEBUG   : Abort message: 'TimeCheck timeout for IAudioFlinger'

Change-Id: I3f4589a89b1a8d00e2f8321d4753e5767fe1b657
Signed-off-by: Jindong <jindong.yue@nxp.com>
Reviewed-on: http://androidsource.ap.freescale.net/project/6812
Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

media/libaudioclient/IAudioFlinger.cpp
@@ -25,6 +25,7 @@
 #include <binder/IPCThreadState.h>
 #include <binder/Parcel.h>
 #include <cutils/multiuser.h>
+#include <cutils/properties.h>
 #include <media/TimeCheck.h>
 #include <private/android_filesystem_config.h>
 
@@ -951,7 +952,12 @@ status_t BnAudioFlinger::onTransact(
             break;
     }
 
-    TimeCheck check("IAudioFlinger");
+    int lpa_enable = property_get_int32("vendor.audio.lpa.enable", 0);
+    if (!lpa_enable) {
+        TimeCheck check("IAudioFlinger");
+    } else {
+        ALOGD("IAudioFlinger: disable timecheck in LPA mode");
+    }
 
     switch (code) {
         case CREATE_TRACK: {

commit 4d01f96e5d09795b2320a8db68a61840071b129b

MA-14128 [Android Pi] The time check is invalid for none-LPA audio
The issue is introduced by
commit b4bddc3038f6097d8a9f5f62628f45b52c71080d
Author: Jindong <jindong.yue@nxp.com>
Date:   Thu Dec 6 10:16:06 2018 +0800

    MA-13701 audio: disable IAudioFlinger timecheck in LPA mode

The Timecheck depends on it's destructor to release the mutex that hold
in it's threadLoop(). In commit b4bddc3, the TimeCheck object life
cycle is just in the if clause. The below code time is not measured.

Change-Id: Ia3654c397d87cc3d0f4b2e2edb7dcadc83b34a29
Signed-off-by: fang hui <hui.fang@nxp.com>
Reviewed-on: http://androidsource.ap.freescale.net/project/7454
Reviewed-by: Jindong Yue <jindong.yue@nxp.com>
(cherry picked from commit 120bca457b1ebc9a1c18bc6f8dd22dd4ad059b3e)
Reviewed-on: http://androidsource.ap.freescale.net/project/7460

media/libaudioclient/IAudioFlinger.cpp
@@ -952,13 +952,22 @@ status_t BnAudioFlinger::onTransact(
             break;
     }
 
+    status_t status;
     int lpa_enable = property_get_int32("vendor.audio.lpa.enable", 0);
-    if (!lpa_enable) {
-        TimeCheck check("IAudioFlinger");
-    } else {
+    if(lpa_enable) {
         ALOGD("IAudioFlinger: disable timecheck in LPA mode");
+        status = onAudioTransact(code, data, reply, flags);
+    } else {
+        TimeCheck check("IAudioFlinger");
+        status = onAudioTransact(code, data, reply, flags);
     }
 
+    return status;
+}
+
+status_t BnAudioFlinger::onAudioTransact(
+    uint32_t code, const Parcel& data, Parcel* reply, uint32_t flags)
+{
     switch (code) {
         case CREATE_TRACK: {
             CHECK_INTERFACE(IAudioFlinger, data, reply);

media/libaudioclient/include/media/IAudioFlinger.h
@@ -506,6 +506,11 @@ public:
 
     // Requests media.log to start merging log buffers
     virtual void requestLogMerge() = 0;
+
+    virtual status_t    onAudioTransact( uint32_t code,
+                                    const Parcel& data,
+                                    Parcel* reply,
+                                    uint32_t flags);
 };
 
 // ----------------------------------------------------------------------------