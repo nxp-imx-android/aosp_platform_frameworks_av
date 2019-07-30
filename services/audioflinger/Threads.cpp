897259cd573fbb33810504ed9e9f13901d1f51ee
Jindong <jindong.yue@nxp.com>
2019-07-30 15:02:27 +0800

    MA-12807 lpa: remove wake_lock in AudioFlinger
    At default, AudioFlinger will hold wake_lock
    when playing audio.
    
    To make system enter suspend mode, use below
    command to enable lpa:
    - setprop vendor.audio.lpa.enable 1
    - pkill audioserver
    
    Change-Id: I2b98d48d46b3ea58f12aa3de7e2ffd96182585bd
    Signed-off-by: Jindong <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/9624
    Reviewed-by: fang hui <hui.fang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/9887
    Reviewed-on: http://androidsource.ap.freescale.net/project/13443
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/17203
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/18616
    Reviewed-by: Ming Zhou <ming.zhou@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/19656

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -542,6 +542,7 @@ AudioFlinger::ThreadBase::ThreadBase(const sp<AudioFlinger>& audioFlinger, audio
         mSignalPending(false)
 {
     mThreadMetrics.logConstructor(getpid(), threadTypeToString(type), id);
+    m_lpa_enable = property_get_int32("vendor.audio.lpa.enable", 0);
     memset(&mPatch, 0, sizeof(struct audio_patch));
 }
 
@@ -1078,6 +1079,9 @@ String16 AudioFlinger::ThreadBase::getWakeLockTag()
 
 void AudioFlinger::ThreadBase::acquireWakeLock_l()
 {
+    if (m_lpa_enable == 1)
+        return;
+
     getPowerManager_l();
     if (mPowerManager != 0) {
         sp<IBinder> binder = new BBinder();
