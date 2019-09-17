674d358af9d2e990312b52ada5cfb4d9d559ce4e
ming.zhou <ming.zhou@nxp.com>
2019-09-17 15:37:51 +0800

    MA-13598: add OMX_COLOR_FormatYCbYCr to supported color format list to pass some cts test. MA-11706-20: add RGB565 format for MediaCodec to pass the cts OMX_COLOR_FormatYCbYCr is default format in amphion vpu. thunbmail needs to convert this format to OMX_COLOR_Format16bitRGB565.
    Change-Id: I34164e0f9d18d6d3abbd9475951c585a609fa211
    Signed-off-by: ming.zhou <ming.zhou@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/10059
    Reviewed-by: mao xu <mao.xu@nxp.com>

--- media/libstagefright/ACodec.cpp
+++ media/libstagefright/ACodec.cpp
@@ -3304,9 +3304,10 @@ status_t ACodec::setSupportedOutputFormat(bool getLegacyFlexibleFormat) {
                 || format.eColorFormat == OMX_COLOR_FormatYUV420SemiPlanar
                 || format.eColorFormat == OMX_COLOR_FormatYUV420PackedSemiPlanar
                 || format.eColorFormat == OMX_TI_COLOR_FormatYUV420PackedSemiPlanar
-                || format.eColorFormat == OMX_COLOR_FormatYUV420SemiPlanar8x128Tiled
+                || format.eColorFormat == OMX_COLOR_FormatYCbYCr
                 || format.eColorFormat == OMX_COLOR_FormatYUV420SemiPlanar8x4Tiled
                 || format.eColorFormat == OMX_COLOR_FormatYUV420SemiPlanar4x4Tiled
+                || format.eColorFormat == OMX_COLOR_Format16bitRGB565
                 || format.eColorFormat == OMX_COLOR_FormatYUV420SemiPlanar4x4TiledCompressed) {
             break;
         }
